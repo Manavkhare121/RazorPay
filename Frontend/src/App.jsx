e750318aaf12957e048fdb4d1cfe0a5a8ab786bb
Manav Khare <manavkhare121@gmail.com>
2026-03-13 17:59:49 +0530

    add axios for API calls and implement product display with purchase functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/products/get-item")
+      .then((response) => {
+        setProduct(response.data.products);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleBuy = () => {
+    alert("Product purchased successfully!");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      {product && (
+        <div className="product-card">
+
+          <img
+            src={product.image}
+            alt={product.title}
+            className="product-image"
+          />
+
+          <div className="product-details">
+            <h2>{product.title}</h2>
+            <p className="description">
+              {product.description}
+            </p>
+            <div className="price">
+              {product.price.amount/100} {product.price.currency}
+            </div>
+            <button
+              className="buy-btn"
+              onClick={handleBuy}
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
